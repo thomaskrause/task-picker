cd9ed24b5bab44a1234fb7f33a007f8ba2dbef14
Thomas Krause <thomaskrause@posteo.de>
2023-03-09 08:18:37 +0100

    Show red background color for overdue task

--- src/app.rs
+++ src/app.rs
@@ -127,8 +127,11 @@ impl TaskPickerApp {
             let mut task_counter = 0;
             for task in self.task_manager.tasks() {
                 let mut group = egui::Frame::group(ui.style());
+                let overdue = task.due.filter(|d| d.cmp(&Utc::now()).is_le()).is_some();
                 if Some(task_counter) == self.selected_task {
                     group.fill = Color32::DARK_BLUE;
+                } else if overdue {
+                    group.fill = Color32::DARK_RED;
                 }
                 group.show(ui, |ui| {
                     let size = Vec2::new(220.0, 250.0);
@@ -161,10 +164,12 @@ impl TaskPickerApp {
                         if let Some(due) = &task.due {
                             let mut due_label = RichText::new(format!("Due: {}", due.to_rfc2822()));
                             let days_to_finish = due.signed_duration_since(Utc::now()).num_days();
-                            if days_to_finish < 0 {
-                                due_label = due_label.color(Color32::RED)
-                            } else if days_to_finish < 1 {
-                                due_label = due_label.color(Color32::GOLD)
+                            if overdue {
+                               due_label = due_label.color(Color32::WHITE);
+                            } else if days_to_finish <= 1 {
+                                due_label = due_label.color(Color32::RED);
+                            } else if days_to_finish <= 2 {
+                                due_label = due_label.color(Color32::GOLD);
                             };
                             ui.label(due_label);
                         }
