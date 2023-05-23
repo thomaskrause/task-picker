8f97153ffcee4bb56db5980f39412ff1adac7334
Thomas Krause <thomaskrause@posteo.de>
2023-05-23 10:08:33 +0200

    Use origin git branch with the necessary changes for setting the app_id

--- src/bin/taskpicker.rs
+++ src/bin/taskpicker.rs
@@ -20,10 +20,8 @@ fn main() -> eframe::Result<()> {
             rgba: icon.into_raw(),
         };
         native_options.icon_data = Some(icon_data);
-        #[cfg(target_os = "linux")]
-        {
-            native_options.app_id = Some("taskpicker".to_string());
-        }
+        native_options.app_id = Some("taskpicker".to_string());
+        
     }
     eframe::run_native(
         "Task Picker",
